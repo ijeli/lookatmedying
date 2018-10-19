796e9eb3cf3b32412029e648bfafd4a3037536e1
Jeremy Legaspi <36684300+ijeli@users.noreply.github.com>
2018-10-18 20:28:22 -0400

    Revert "Devchris"

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactModalLogin from 'react-modal-login';
 import {facebookConfig, googleConfig} from "./social-config";
 import PrivacyPolicy from './PrivacyPolicy';
-
  
 class LoginModal extends React.Component {
  
@@ -27,19 +26,14 @@ class LoginModal extends React.Component {
     this.setState({
       showModal: false,
       error: null,
+      Policy: false
     });
-  }
-
-  nextPath(path) {
-    this.props.history.push(path)
   }
   
   onLoginSuccess(method, response) {
     console.log('logged successfully with ' + method);
-    if(response.data.code === 200){
-      this.nextPath('/home');
-    };
-}
+    
+  }
  
   onLoginFail(method, response) {
     console.log('logging failed with ' + method);
