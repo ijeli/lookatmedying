6efa5ca9c1ecf0509b9bda36fba89196c236f73d
ijeli <legaspi.jel@gmail.com>
2018-10-20 10:13:17 -0400

    added symptom to user table

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ module.exports = function(sequelize, DataTypes) {
         lastName: DataTypes.STRING,
         monthOfBrith: DataTypes.INTEGER,
         dayOfBirth: DataTypes.INTEGER,
-        yearOfBirth: DataTypes.INTEGER
+        yearOfBirth: DataTypes.INTEGER,
+        symptom: DataTypes.STRING
     });
     return User;
 }
